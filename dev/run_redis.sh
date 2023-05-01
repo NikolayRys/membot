cebfad058e04973e77bcdcfbac7c216dc86a3b16
Vadim Pushtaev <pushtaev.vm@gmail.com>
2023-05-01 15:53:23 +0000

    redis: listen 127.0.0.1 only

--- dev/run_redis.sh
+++ dev/run_redis.sh
@@ -1,1 +1,1 @@
-docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
+docker run -d --name redis-stack -p 127.0.0.1:6379:6379 -p 127.0.0.1:8001:8001 redis/redis-stack:latest
